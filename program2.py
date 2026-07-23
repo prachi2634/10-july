e727814609b202505795635212be0bfc97c9fd53
prachi2634 <prachibhavsar3407@gmail.com>
2026-07-23 21:05:47 +0530

    today's project

---
+++ program2.py
@@ -0,0 +1,11 @@
+
+
+
+a = float(input("Enter number 1: "))
+b = float(input("Enter number 2: "))
+c = float(input("Enter number 3: "))
+d = float(input("Enter number 4: "))
+e = float(input("Enter number 5: "))
+
+avg = (a + b + c + d + e) / 5
+print(f"Average = {avg:.2f}")
